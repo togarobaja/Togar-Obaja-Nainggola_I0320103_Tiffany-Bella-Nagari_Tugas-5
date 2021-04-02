f45ee7f0037d20188f4aec967982ed2550317043
Togar Obaja <80144717+togarobaja@users.noreply.github.com>
2021-04-02 15:28:55 +0700

    Add files via upload

---
+++ Soal 1.py
@@ -0,0 +1,8 @@
+# Robot penyambut tamu hotel
+nama = input(str('Masukkan nama Anda :'))
+gender = input(str("Masukkan gender anda (L/P):"))
+if gender == 'L' :
+    print("Selamat Datang, tuan",nama,'!')
+else :
+    print("Selamat Datang, nyonya",nama,'!')
+print()
